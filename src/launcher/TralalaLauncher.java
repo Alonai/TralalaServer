e3a5bc3f8b93d21ca3728be50612f7140a93ba46
Seven Flying <diez.f.irene@gmail.com>
2014-01-16 15:22:23 +0100

    Datos para hacer lookup y rebind en launcher + build con todos los args

--- src/launcher/TralalaLauncher.java
+++ src/launcher/TralalaLauncher.java
@@ -3,5 +3,17 @@ package launcher;
 /** Main entry
  */
 public class TralalaLauncher {
-	
+	public static void main(String[] argv){
+		// Datos para hacer rebind en facades
+		String namePayFac= "//" + argv[0] + ":" + argv[1] + "/" + argv[2];
+		String nameSongFac = "//" + argv[3] + ":" + argv[4] + "/" + argv[5];
+		String nameManageFac = "//" + argv[6] + ":" + argv[7] + "/" + argv[8];
+		
+		// Datos para hacer lookup en gateways
+		String auth=  "//" + argv[9] + ":" + argv[10] + "/" + argv[11];
+		String paypal=  "//" + argv[12] + ":" + argv[13] + "/" + argv[14];
+		String banking=  "//" + argv[15] + ":" + argv[16] + "/" + argv[17];
+		
+		
+	}
 }
