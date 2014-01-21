332b1a106b21309a491b40113bed09c5702494d0
Alchemy-Meister <jesusesma@gmail.com>
2014-01-21 23:03:28 +0100

    login working

--- src/remote/IManagementSessionFacade.java
+++ src/remote/IManagementSessionFacade.java
@@ -6,5 +6,6 @@ import java.rmi.RemoteException;
 public interface IManagementSessionFacade extends Remote{
 	
 	public boolean signIn(String user, String pass) throws RemoteException;
+	public boolean registerUser(String nick, String pass) throws RemoteException;
 
 }
